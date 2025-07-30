9d45f848e541244095bb73bf8e664fd15607109f
gosangam <sangam4742@gmail.com>
2025-07-30 21:06:00 +0530

    padding

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -28,6 +28,7 @@ const Search = ({ onSearch }: { onSearch: (query: number) => void }) => {
   return (
     <div
       style={{
+        padding: '1rem',
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
